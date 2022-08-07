99bb0f72035cbdb607b800f3c95465e46b28881f
ScoBoFoSho <ferrecisco@icloud.com>
2022-08-07 12:20:13 -0400

    generate an HTML file

--- app.js
+++ app.js
@@ -5,26 +5,10 @@ const [name, github] = profileDataArgs;
 // the necessary object that allows us to access file system module
 const fs = require("fs");
 
-const generatePage = (userName, githubName) => {
-  return `
-    <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+const generatePage = require("./src/page-template.js");
 
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
-
-fs.writeFile("index.html", generatePage(name, github), (err) => {
+const pageHtml = generatePage(name, github);
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
   if (err) throw err;
 
   console.log("Portfolio complete! Check out index.html to see the output!");
