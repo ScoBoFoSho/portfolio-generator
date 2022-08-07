adf9899125018e3c14af6afe67a834cd6c6535c9
ScoBoFoSho <ferrecisco@icloud.com>
2022-08-07 13:22:08 -0400

    inquirer package installed/initial prompt

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-// the array that collects our data
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// takes in user input from command line
-const [name, github] = profileDataArgs;
-// the necessary object that allows us to access file system module
-const fs = require("fs");
+const inquirer = require("inquirer");
 
-const generatePage = require("./src/page-template.js");
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
 
-const pageHtml = generatePage(name, github);
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw err;
+// const [name, github] = profileDataArgs;
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// const fs = require("fs");
+
+// const generatePage = require("./src/page-template.js");
+
+// const pageHtml = generatePage(name, github);
+// fs.writeFile("./index.html", generatePage(name, github), (err) => {
+//   if (err) throw err;
+
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
